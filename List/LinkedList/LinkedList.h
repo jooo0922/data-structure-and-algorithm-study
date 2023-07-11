f521302d1ed388ad7e84186c7501f9c3d0c5ccf5
jooo0922 <jooo0922@gmail.com>
2023-07-12 00:42:28 +0900

    feat: 링크드리스트 연산 헤더파일 선언

---
+++ List/LinkedList/LinkedList.h
@@ -0,0 +1,31 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+/*
+	여기서 사용된 #ifndef ~ #endif 전처리기는
+	헤더파일인 LinkedList.h 의 헤더가드를 처리해주는 방법 중 하나였지?
+
+	cpp-study > TBCppStudy > Chapter1_12 > add.h 필기 참고!
+*/
+
+// 노드에 들어갈 데이터 타입. 여기서는 정수형으로 임의로 정함.
+typedef int ElementType; 
+
+// 노드 구조체 선언 (struct 키워드 생략하고 사용하기 위해 typedef 로 구조체 별칭 선언)
+typedef struct tagNode
+{
+	ElementType Data; // 노드에 들어갈 데이터
+	struct tagNode* NextNode; // 링크드리스트 상에서 다음 노드 구조체를 가리키는 포인터 멤버변수
+} Node;
+
+// 함수 원형 선언
+Node* SLL_CreateNode(ElementType NewData); // 최초 노드(헤드노드) 생성
+void SLL_DestroyNode(Node* Node); // 노드 파괴(메모리 해제)
+void SLL_AppendNode(Node** Head, Node* NewNode); // 노드 추가 (테일노드에 추가)
+void SLL_InsertAfter(Node* Current, Node* NewNode); // 노드 삽입
+void SLL_InsertNewHead(Node** Head, Node* NewHead); // 새로운 헤드노드 삽입
+void SLL_RemoveNode(Node** Head, Node* Remove); // 링크드리스트에서 노드 제거
+Node* SLL_GetNodeAt(Node* Head, int Location); // 노드 탐색
+int SLL_GetNodeCount(Node* Head); // 노드 개수 세기
+
+#endif // !LINKEDLIST_H
