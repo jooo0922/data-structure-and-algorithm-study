fea238b9449faa1dd12485152d445b4d2295580e
jooo0922 <jooo0922@gmail.com>
2023-07-15 13:39:47 +0900

    rename: LinkedList.h 헤더파일 참조 이동

--- List/LinkedList/LinkedList.h
+++ List/LinkedList/LinkedList.h
@@ -9,7 +9,7 @@
 */
 
 // 노드에 들어갈 데이터 타입. 여기서는 정수형으로 임의로 정함.
-typedef int ElementType; 
+typedef int ElementType;
 
 // 노드 구조체 선언 (struct 키워드 생략하고 사용하기 위해 typedef 로 구조체 별칭 선언)
 typedef struct tagNode
