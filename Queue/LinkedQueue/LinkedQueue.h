e762f03cfd3a905e1435967f93792e7812d7a296
jooo0922 <jooo0922@gmail.com>
2023-09-20 21:54:34 +0900

    fix: stdio.h 라이브러리 추가

--- Queue/LinkedQueue/LinkedQueue.h
+++ Queue/LinkedQueue/LinkedQueue.h
@@ -7,6 +7,7 @@
 	cpp-study > TBCppStudy > Chapter1_12 > add.h 필기 참고!
 */
 
+#include <stdio.h> // printf(), scanf() 같은 입출력 함수 선언이 포함된 헤더파일
 #include <stdint.h> // printf(), scanf() 같은 입출력 함수 선언이 포함된 헤더파일
 #include <string.h> // strlen(), strcpy() 같은 문자열 처리 함수 선언이 포함된 헤더파일
 #include <stdlib.h> // malloc(), free() 같은 메모리 할당 함수 선언이 포함된 헤더파일
