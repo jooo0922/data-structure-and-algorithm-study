334d1d610fc19f6c36e3a7ec564c930d5a5b2438
jooo0922 <jooo0922@gmail.com>
2023-10-07 16:07:56 +0900

    feat: 수식트리 함수 구현부(몸체) 선언

---
+++ Tree/ExpressionTree/ExpressionTree.c
@@ -0,0 +1,49 @@
+#include "ExpressionTree.h"
+
+// 수식트리 노드 생성
+ETNode* ET_CreateNode(ElementType NewData)
+{
+
+}
+
+// 수식트리 노드 메모리 해제
+void ET_DestroyNode(ETNode* Node)
+{
+
+}
+
+// 수식트리 뿌리노드 메모리 해제 > 수식트리 전체 메모리 해제
+void ET_DestroyTree(ETNode* Root)
+{
+
+}
+
+// 수식트리 전위순회 출력 > 전위표기식 출력
+void ET_PreorderPrintTree(ETNode* Node)
+{
+
+}
+
+// 수식트리 중위순회 출력 > 중위표기식 출력
+void ET_InorderPrintTree(ETNode* Node)
+{
+
+}
+
+// 수식트리 후위순회 출력 > 후위표기식 출력
+void ET_PostorderPrintTree(ETNode* Node)
+{
+
+}
+
+// 수식트리 구축
+void ET_BuildExpressionTree(char* PostfixExpression, ETNode** Node)
+{
+
+}
+
+// 수식트리 계산
+double ET_Evaluate(ETNode* Tree)
+{
+
+}
