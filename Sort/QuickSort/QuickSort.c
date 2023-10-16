4ee32165e0be8db48c1ff9566d2c4487e70c93f8
jooo0922 <jooo0922@gmail.com>
2023-10-16 23:20:51 +0900

    feat: Partition() 함수 구현

--- Sort/QuickSort/QuickSort.c
+++ Sort/QuickSort/QuickSort.c
@@ -12,7 +12,45 @@ void Swap(int* A, int* B)
 // Left 는 왼쪽 정찰병 위치, Right 는 오른쪽 정찰병 위치
 int Partition(int DataSet[], int Left, int Right)
 {
+    int First = Left; // 맨 왼쪽 정찰병의 초기 위치 = 그룹의 첫 번째 요소 위치
+    int Pivot = DataSet[First]; // 기준 요소를 그룹의 첫 번째 요소로 지정
 
+    ++Left; // 왼쪽 정찰병의 시작 위치를 첫 번째 요소 다음 요소부터 시작하도록 지정
+
+    // 왼쪽 정찰병(Left)와 오른쪽 정찰병(Right)이 접선할 때까지 그룹 요소를 순회
+    while (Left <= Right)
+    {
+        // 왼쪽 정찰병이 기준요소보다 큰 요소를 발견할 때까지 왼쪽 정찰병 위치(Left) 이동
+        while (DataSet[Left] <= Pivot && Left < Right)
+        {
+            ++Left;
+        }
+
+        // 오른쪽 정찰병이 기즌요소보다 작은 요소를 발견할 때까지 오른쪽 정찰병 위치(Right) 이동
+        while (DataSet[Right] >= Pivot && Left <= Right)
+        {
+            --Right;
+        }
+
+        if (Left < Right)
+        {
+            // 두 정찰병이 아직 접선(cross)하지 않은 상태이면,
+            // 두 정찰병이 찾은 기준요소보다 큰 요소와 작은 요소를 맞교환함
+            Swap(&DataSet[Left], &DataSet[Right]);
+        }
+        else
+        {
+            // 두 정찰병이 접선한 상태 (Left >= Right) 이므로, 현재 그룹분할 종료
+            break;
+        }
+    }
+
+    // 그룹분할 종료 후, 기준요소인 첫 번째 요소와 왼쪽 그룹의 마지막 요소의 위치 맞교환
+    // 참고로, 현재 Left >= Right 상태이므로, 왼쪽 그룹의 마지막 요소 위치는 Right 에 저장되어 있음! 
+    Swap(&DataSet[First], &DataSet[Right]);
+
+    // 새로운 기준이 될 요소 위치 반환
+    return Right;
 }
 
 // 퀵 정렬 함수 구현
