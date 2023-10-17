b6309012594b9fda32212c7196d48a78d2eeec33
jooo0922 <jooo0922@gmail.com>
2023-10-18 00:07:52 +0900

    feat: 내림차순 정렬을 위한 비교함수 구현

--- Sort/QuickSort2/QuickSort2.c
+++ Sort/QuickSort2/QuickSort2.c
@@ -28,6 +28,32 @@ int ComparePoint(const void* _elem1, const void* _elem2)
     }
 }
 
+// 내림차순으로 정렬하는 비교함수 선언 및 구현
+// 구글에 "JS sort() 내림차순 정렬" 검색하면 비교함수를 어떻게 구현하는지 다 나옴.. 
+int ComparePointDescend(const void* _elem1, const void* _elem2)
+{
+    // 두 void* 타입의 주소값을 int* 타입으로 형변환하여 int* 타입 포인터 변수에 복사
+    int* elem1 = (int*)_elem1;
+    int* elem2 = (int*)_elem2;
+
+    // 두 포인터 변수를 de-referencing 하여 두 정수 타입의 크기를 비교함
+    if (*elem1 > *elem2)
+    {
+        // 첫 번째 요소가 두 번째 요소보다 크면 오름차순과 반대로 -1 반환
+        return -1;
+    }
+    else if (*elem1 < elem2)
+    {
+        // 첫 번째 요소가 두 번째 요소보다 작다면 오름차순과 반대로 1 반환
+        return 1;
+    }
+    else
+    {
+        // 첫 번째 요소와 두 번째 요소가 같다면 0을 반환
+        return 0;
+    }
+}
+
 int main()
 {
     int DataSet[] = { 6, 4, 2, 3, 1, 5 }; // 퀵 정렬할 정적 배열 선언 및 초기화
@@ -37,7 +63,7 @@ int main()
     // c언어 표준 라이브러리 qsort() 을 사용한 퀵 정렬 수행 (각 매개변수 설명 p.227 참고)
     // 첫 번째 매개변수는 void* 타입 주소값만 받기 때문에, 
     // int 타입 정적 배열의 주소값을 void* 타입으로 형변환해서 전달한 것!
-    qsort((void*)DataSet, Length, sizeof(int), ComparePoint);
+    qsort((void*)DataSet, Length, sizeof(int), ComparePointDescend);
 
     // std::qsort() 를 사용한 퀵 정렬 결과 출력
     for (i = 0; i < Length; i++)
