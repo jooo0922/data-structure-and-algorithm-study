5ada5b736d3ad3dbd8215f392d1b7f434665a939
jooo0922 <jooo0922@gmail.com>
2023-10-21 11:47:07 +0900

    feat: DoubleLinkedList 코드 추가

---
+++ Sort/InsertionSort_Exercise/DoubleLinkedList.c
@@ -0,0 +1,178 @@
+#include "DoubleLinkedList.h" // 헤더파일 포함
+
+// 노드 생성
+Node* DLL_CreateNode(ElementType NewData)
+{
+	Node* NewNode = (Node*)malloc(sizeof(Node)); // 힙 메모리에 Node 포인터 타입에 해당하는 메모리 공간 할당
+
+	NewNode->Data = NewData; // 포인터 멤버변수 참조로 데이터 저장
+	NewNode->PrevNode = NULL; // 포인터 멤버변수 참조로 이전 노드에 대한 포인터는 NULL 로 초기화
+	NewNode->NextNode = NULL; // 포인터 멤버변수 참조로 다음 노드에 대한 포인터는 NULL 로 초기화
+
+	return NewNode; // 생성한 노드 포인터(주소값) 반환
+}
+
+// 노드 소멸(메모리에서 아예 제거)
+void DLL_DestroyNode(Node* Node)
+{
+	free(Node); // 힙 메모리에서 전달받은 Node 포인터가 가리키는 메모리 해제
+}
+
+// 노드 추가 (테일 노드에 추가)
+void DLL_AppendNode(Node** Head, Node* NewNode)
+{
+	// 헤드 노드가 NULL 이면 인자로 전달받은 새로운 노드가 헤드 노드가 되도록 주소값을 할당
+	// *Head 는 "포인터 변수 Head 가 가리키는 메모리 공간에 저장된 값, 가리키는 대상"을 의미! 이를 "역참조 연산자" 라고 함.
+	// 즉, Head 포인터 변수는 Node 포인터 변수(Node*)를 가리키는 이중 포인터 변수이므로,
+	// *Head 는 Node* 포인터 변수(외부 포인터 변수)를 가리킨다고 보면 됨!
+	if ((*Head) == NULL)
+	{
+		*Head = NewNode; // Head 포인터가 가리키는 대상인 또 다른 (외부) 포인터 변수 Node* 에 NewNode 주소값 할당
+	}
+	else
+	{
+		// 테일 노드를 찾는다
+		Node* Tail = (*Head); // 반복 순회의 첫 번째 노드를 헤드노드로 지정
+		while (Tail->NextNode != NULL)
+		{
+			// NextNode 포인터 멤버변수가 아무것도 가리키지 않는 노드가 테일 노드이니,
+			// 테일 노드를 찾을 때까지 계속 덮어씀
+			Tail = Tail->NextNode;
+		}
+
+		Tail->NextNode = NewNode; // 최종적으로 찾은 테일노드의 NextNode 포인터가 NewNode 를 가리키도록 주소값 할당
+		NewNode->PrevNode = Tail; // NewNode 의 PrevNode 포인터는 기존 테일노드를 가리키도록 함. 
+	}
+}
+
+// 노드 삽입
+void DLL_InsertAfter(Node* Current, Node* NewNode)
+{
+	NewNode->NextNode = Current->NextNode; // 새 노드의 NextNode 가 현재 노드의 NextNode 를 가리키게 함.
+	NewNode->PrevNode = Current; // 새 노드의 PrevNode 는 현재 노드(이전노드)를 가리키도록 함.
+
+	// 현재 노드의 다음 노드가 존재하는지 체크 (why? 마지막 노드 뒤에 '삽입'하는 걸수도 있잖아!)
+	if (Current->NextNode != NULL)
+	{
+		Current->NextNode->PrevNode = NewNode; // 다음 노드의 PreNode 포인터가 새로운 노드를 가리키도록 함.
+		Current->NextNode = NewNode; // 현재 노드(이전노드)의 NextNode 포인터도 새로운 노드를 가리키도록 함.
+	}
+}
+
+// 노드 제거(링크드리스트 내에서만 제거) 
+void DLL_RemoveNode(Node** Head, Node* Remove)
+{
+	if (*Head == Remove)
+	{
+		// 헤드노드를 가리키는 포인터 변수와 제거할 노드를 가리키는 포인터 변수가 동일하다면,
+		// 제거할 노드가 현재의 헤드노드 그 자체이기 때문에,
+		// 그냥 헤드노드를 가리키는 포인터 변수가, 현재 헤드노드의 다음 노드를 가리키도록 바꿔주기만 하면 됨!
+		*Head = Remove->NextNode;
+
+		// 헤드노드가 방금 변경되었기 때문에, 
+		// 기본적으로 더블 링크드리스트에서 모든 헤드노드는 PrevNode 포인터가 아무것도 가리키지 않도록 NULL 초기화를 해줘야 함.
+		if ((*Head) != NULL)
+		{
+			(*Head)->PrevNode = NULL; // 교체된 헤드노드의 PrevNode 포인터는 NULL 로 초기화!
+		}
+
+		// 리스트에서 삭제할 노드의 포인터 멤버변수들은 항상 NULL 초기화를 해줘야 함!
+		Remove->PrevNode = NULL;
+		Remove->NextNode = NULL;
+	}
+	else
+	{
+		// 헤드노드와 제거할 노드가 서로 다를 경우!
+
+		// 제거할 노드의 참조를 다른 포인터 변수에도 저장해두기
+		Node* Temp = Remove;
+
+		if (Remove->PrevNode != NULL)
+		{
+			// 제거할 노드의 이전 노드의 NextNode 포인터가 제거할 노드(Temp)의 다음 노드를 가리키도록!
+			Remove->PrevNode->NextNode = Temp->NextNode;
+		}
+
+		if (Remove->NextNode != NULL)
+		{
+			// 제거할 노드의 다음 노드의 PrevNode 포인터가 제거할 노드(Temp)의 이전 노드를 가리키도록!
+			Remove->NextNode->PrevNode = Temp->PrevNode;
+		}
+
+		// 리스트에서 삭제할 노드의 포인터 멤버변수들은 항상 NULL 초기화를 해줘야 함!
+		Remove->PrevNode = NULL;
+		Remove->NextNode = NULL;
+	}
+}
+
+// 노드 탐색
+Node* DLL_GetNodeAt(Node* Head, int Location)
+{
+	Node* Current = Head; // 반복순회할 현재 노드가 헤드노드를 가리키도록 포인터 변수 초기화
+
+	while (Current != NULL && (--Location) >= 0)
+	{
+		// 사용자가 전달한 인덱스(Location) 을 1씩 차감하며 원하는 노드까지 순서대로 접근 
+		Current = Current->NextNode;
+	}
+
+	return Current;
+}
+
+// 노드 개수 세기
+int DLL_GetNodeCount(Node* Head)
+{
+	unsigned int Count = 0; // 개수는 양수이므로, 부호가 없는 정수형 타입 변수로 초기화해도 충분하겠지? -> 메모리 공간 절약!
+	Node* Current = Head; // 현재 노드가 헤드노드를 가리키도록 포인터 변수 주소값 초기화
+
+	while (Current != NULL)
+	{
+		Current = Current->NextNode; // 현재 노드를 다음 노드로 덮어쓰면서 테일 노드까지 계속 순회
+		Count++; // 순회하면서 개수 변수를 1씩 증가
+	}
+
+	return Count;
+}
+
+void PrintNode(Node* _Node)
+{
+	if (_Node->PrevNode == NULL)
+	{
+		// PrevNode 포인터가 가리키는 노드가 없는 경우 (즉, 헤드노드인 경우)
+		printf("Prev: NULL");
+	}
+	else
+	{
+		printf("Prev: %d", _Node->PrevNode->Data); // 현재 노드의 PrevNode 가 가리키는 노드의 데이터 출력
+		printf("Current: %d", _Node->Data); // 현재 노드의 데이터 출력
+	}
+
+	if (_Node->NextNode == NULL)
+	{
+		// NextNode 포인터가 가리키는 노드가 없는 경우 (즉, 테일노드인 경우)
+		printf("Next: NULL\n");
+	}
+	else
+	{
+		printf("Next: %d\n", _Node->NextNode->Data);
+	}
+}
+
+void PrintReverse(Node* Head)
+{
+	Node* Tail = Head; // 테일노드가 헤드노드를 가리키도록 포인터 변수 주소값 초기화
+
+	// NextNode 가 NULL 을 가리키는 노드(테일노드)가 나올때까지 Tail 에 덮어쓰며 반복 순회
+	while (Tail->NextNode != NULL)
+	{
+		Tail = Tail->NextNode;
+	}
+
+	Node* Current = Tail; // 현재노드가 위에 while 문에서 찾은 테일노드를 가리키도록 포인터 변수 초기화
+
+	while (Current != NULL)
+	{
+		printf("Current: %d\n", Current->Data); // 현재 노드 데이터 출력
+		Current = Current->PrevNode; // 현재 노드를 이전 노드로 덮어쓰면서 헤드노드까지 계속 순회
+	}
+}
---
+++ Sort/InsertionSort_Exercise/DoubleLinkedList.h
@@ -0,0 +1,35 @@
+#ifndef  DOUBLE_LINKEDLIST_H
+#define DOUBLE_LINKEDLIST_H
+
+/*
+	헤더파일 중복 include 에 대한 헤더가드를 처리해주는 방법 중 하나였지?
+	cpp-study > TBCppStudy > Chapter1_12 > add.h 필기 참고!
+*/
+
+#include <stdio.h> // printf(), scanf() 같은 입출력 함수 선언이 포함된 헤더파일
+#include <stdlib.h> // malloc(), free() 같은 메모리 할당 함수 선언이 포함된 헤더파일
+// -> 헤더파일에 포함되어 있어야, 이를 가져다 쓰는 .c 파일에서 malloc() 등 사용할 때 에러가 안남.
+
+// 노드에 들어갈 데이터 타입. 여기서는 정수형으로 임의로 정함.
+typedef int ElementType;
+
+// 노드 구조체 선언 (struct 키워드 생략하고 사용하기 위해 typedef 로 구조체 별칭 선언)
+typedef struct tagNode
+{
+	ElementType Data; // 노드에 들어갈 데이터
+	struct tagNode* PrevNode; // 링크드리스트 상에서 이전 노드 구조체를 가리키는 포인터 멤버변수
+	struct tagNode* NextNode; // 링크드리스트 상에서 다음 노드 구조체를 가리키는 포인터 멤버변수
+} Node;
+
+// 함수 원형 선언
+Node* DLL_CreateNode(ElementType NewData); // 최초 노드(헤드노드) 생성
+void DLL_DestroyNode(Node* Node); // 노드 파괴(메모리 해제)
+void DLL_AppendNode(Node** Head, Node* NewNode); // 노드 추가 (테일노드에 추가)
+void DLL_InsertAfter(Node* Current, Node* NewNode); // 노드 삽입
+void DLL_RemoveNode(Node** Head, Node* Remove); // 링크드리스트에서 노드 제거
+Node* DLL_GetNodeAt(Node* Head, int Location); // 노드 탐색
+int DLL_GetNodeCount(Node* Head); // 노드 개수 세기
+void PrintNode(Node* _Node); // 노드 데이터 출력
+void PrintReverse(Node* Head); // 더블 링크드리스트 역순 출력
+
+#endif // ! DOUBLE_LINKEDLIST_H
