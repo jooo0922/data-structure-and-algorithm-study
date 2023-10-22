0aaff5d6e1cc88d61475f109f41d3ddaf1ce2c98
jooo0922 <jooo0922@gmail.com>
2023-10-23 00:24:43 +0900

    comment: 주석 추가

--- Search/LinkedListSequentialSearch/LinkedListSequentialSearch.c
+++ Search/LinkedListSequentialSearch/LinkedListSequentialSearch.c
@@ -27,14 +27,19 @@ int main()
 		printf("List[%d] : %d\n", i, Current->Data); // %d 는 정수값을 입력받은 형식지정자 > 문자열 뒤에 전달한 정수값으로 치환함
 	}
 
+	// 순차탐색으로 3을 데이터로 갖는 노드를 탐색한다는 메시지 출력
 	printf("\nSearch Node which has value of 3 by Sequential Search...\n");
+
+	// 링크드리스트 순차탐색 수행
 	Node* MatchNode = SLL_SequentialSearch(List, 3);
 	if (MatchNode == NULL)
 	{
+		// 순차탐색 결과 해당 노드가 없을 경우, 탐색에 실패했다는 메시지 출력
 		printf("Node Not Found!\n");
 	}
 	else
 	{
+		// 순차탐색 결과 해당 노드를 발견했을 경우, 해당 노드의 데이터와 다음 노드 데이터 출력
 		printf("Match Node : %d\n", MatchNode->Data);
 		printf("Next Node : %d\n", MatchNode->NextNode->Data);
 	}
