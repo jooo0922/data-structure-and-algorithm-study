21af7c35770fcf1b0fbab7b183452df694b03480
jooo0922 <jooo0922@gmail.com>
2023-10-23 00:13:55 +0900

    chore: ArraySequentialSearch 프로젝트 생성

---
+++ Search/ArraySequentialSearch/ArraySequentialSearch.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int SequentialSearch()
+{
+
+}
+
+int main()
+{
+	int DataSet[] = { 6, 4, 2, 3, 1, 5 }; // 순차탐색할 정적 배열 선언 및 초기화
+	int Length = sizeof DataSet / sizeof DataSet[0]; // 정적 배열 길이 계산하여 저장
+
+	return 0;
+}
