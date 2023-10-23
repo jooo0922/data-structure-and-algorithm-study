e95f79aa70280ddfb9255426c4d51d0706cd2c72
jooo0922 <jooo0922@gmail.com>
2023-10-23 22:34:17 +0900

    feat: 전진이동법 순차탐색 테스트 코드 작성

--- Search/LinkedListSequentialSearch/LinkedListSequentialSearch.c
+++ Search/LinkedListSequentialSearch/LinkedListSequentialSearch.c
@@ -21,17 +21,22 @@ int main()
 	for (i = 0; i < Count; i++)
 	{
 		Current = SLL_GetNodeAt(List, i); // 링크드리스트의 각 노드 주소를 찾아서 반환
-
-		// 링크드리스트의 각 노드 인덱스와 데이터 출력
-		// printf() 입출력 표준함수를 사용할 수 있는 이유는, LinkedList.h 헤더파일에 stdio.h 헤더파일을 포함시켜놓은 덕분!
-		printf("List[%d] : %d\n", i, Current->Data); // %d 는 정수값을 입력받은 형식지정자 > 문자열 뒤에 전달한 정수값으로 치환함
+		printf("%d ", Current->Data); // 링크드리스트의 각 노드 데이터 출력
 	}
+	printf("\n");
 
 	// 순차탐색으로 3을 데이터로 갖는 노드를 탐색한다는 메시지 출력
-	printf("\nSearch Node which has value of 3 by Sequential Search...\n");
+	//printf("\nSearch Node which has value of 3 by Sequential Search...\n");
 
 	// 링크드리스트 순차탐색 수행
-	Node* MatchNode = SLL_SequentialSearch(List, 3);
+	//Node* MatchNode = SLL_SequentialSearch(List, 3);
+
+	// 전진이동법 순차탐색으로 3을 데이터로 갖는 노드를 탐색한다는 메시지 출력
+	printf("\nSearch Node which has value of 3 by Sequential Search with MoveToFront...\n");
+
+	// 전진이동법 순차탐색 수행
+	Node* MatchNode = SLL_MoveToFront(&List, 3);
+
 	if (MatchNode == NULL)
 	{
 		// 순차탐색 결과 해당 노드가 없을 경우, 탐색에 실패했다는 메시지 출력
@@ -42,6 +47,14 @@ int main()
 		// 순차탐색 결과 해당 노드를 발견했을 경우, 해당 노드의 데이터와 다음 노드 데이터 출력
 		printf("Match Node : %d\n", MatchNode->Data);
 		printf("Next Node : %d\n", MatchNode->NextNode->Data);
+
+		// 전진이동법으로 정렬된 링크드리스트 출력
+		for (i = 0; i < Count; i++)
+		{
+			Current = SLL_GetNodeAt(List, i); // 링크드리스트의 각 노드 주소를 찾아서 반환
+			printf("%d ", Current->Data); // 링크드리스트의 각 노드 데이터 출력
+		}
+		printf("\n");
 	}
 
 	// 링크드리스트 상의 모든 노드 메모리 해제한다는 메시지 출력
