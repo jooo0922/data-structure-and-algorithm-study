44d6aae379d2dac4bae82c00d1b4b34d2fda8b4e
jooo0922 <jooo0922@gmail.com>
2023-10-24 00:37:34 +0900

    feat: 배열 기반 배수법 순차탐색 구현

--- Search/ArraySequentialSearch/ArraySequentialSearch.c
+++ Search/ArraySequentialSearch/ArraySequentialSearch.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h> // memmove() 사용을 위해 포함
+#include <stdlib.h> // qsort() 퀵 정렬 함수를 사용하기 위해 포함
+
+#include "Node.h"
 
 // 배열 순차탐색 함수
 int ARR_SequentialSearch(int DataSet[], int Length, int Target)
@@ -93,6 +96,57 @@ int ARR_Transpose(int DataSet[], int Length, int Target)
 
 }
 
+// 내림차순으로 정렬하는 비교함수 선언 및 구현
+int CompareNodeDescend(const void* _elem1, const void* _elem2)
+{
+	// 두 void* 타입의 주소값을 Node* 타입으로 형변환하여 Node* 타입 포인터 변수에 복사
+	Node* elem1 = (Node*)_elem1;
+	Node* elem2 = (Node*)_elem2;
+
+	// 두 포인터 변수를 de-referencing 하여 두 Node 구조체의 FrequencyCount 멤버의 값 크기를 비교함
+	if (elem1->FrequencyCount > elem2->FrequencyCount)
+	{
+		// 첫 번째 요소가 두 번째 요소보다 크면 오름차순과 반대로 -1 반환
+		return -1;
+	}
+	else if (elem1->FrequencyCount < elem2->FrequencyCount)
+	{
+		// 첫 번째 요소가 두 번째 요소보다 작다면 오름차순과 반대로 1 반환
+		return 1;
+	}
+	else
+	{
+		// 첫 번째 요소와 두 번째 요소가 같다면 0을 반환
+		return 0;
+	}
+}
+
+// 배열 계수법 순차탐색 함수
+Node* ARR_FrequencyCount(Node NodeSet[], int Length, int Target)
+{
+	Node* Current; // 순차탐색 시 순회할 변수 초기화
+
+	// 정적 배열 길이만큼 순회하며 순차탐색
+	for (int i = 0; i < Length; i++)
+	{
+		Current = &NodeSet[i];
+		if (Current->Data == Target)
+		{
+			// 찾고자 하는 노드를 발견했을 때, 노드의 탐색 횟수 증가
+			Current->FrequencyCount += 1;
+
+			// 원하는 노드를 찾았으니 순차 탐색 종료
+			break;
+		}
+	}
+
+	// 탐색 횟수를 기준으로 내림차순 퀵 정렬 수행
+	qsort((void*)NodeSet, Length, sizeof(Node), CompareNodeDescend);
+
+	// 찾고자 하는 값이 저장된 변수를 반환함.
+	return &NodeSet[0];
+}
+
 int main()
 {
 	int DataSet[] = { 6, 4, 2, 3, 1, 5 }; // 순차탐색할 정적 배열 선언 및 초기화
@@ -139,6 +193,51 @@ int main()
 		for (unsigned int i = 0; i < Length; i++)
 		{
 			printf("%d ", DataSet[i]);
+		}
+		printf("\n");
+	}
+
+
+	/* 배열 기반 계수법 순차탐색 테스트 코드 */
+	int NodeSetLength = sizeof NodeSet / sizeof NodeSet[0]; // 정적 배열 길이 계산하여 저장
+
+	// 탐색할 정적 배열 출력
+	printf("\nPrinting NodeSet...\n");
+	for (int i = 0; i < NodeSetLength; i++)
+	{
+		printf("%d ", NodeSet[i].Data);
+	}
+	printf("\n");
+
+	// 계수법 순차탐색으로 정적 배열에서 3을 탐색한다는 메시지 출력
+	printf("\nSearch Node which has value of 3 by Sequential Search with FrequencyCount...\n");
+
+	// 배열 순차탐색 수행
+	Node* MatchNode = ARR_FrequencyCount(NodeSet, NodeSetLength, 3);
+	MatchNode = ARR_FrequencyCount(NodeSet, NodeSetLength, 6);
+	MatchNode = ARR_FrequencyCount(NodeSet, NodeSetLength, 2);
+	MatchNode = ARR_FrequencyCount(NodeSet, NodeSetLength, 2);
+	MatchNode = ARR_FrequencyCount(NodeSet, NodeSetLength, 3);
+	MatchNode = ARR_FrequencyCount(NodeSet, NodeSetLength, 6);
+	MatchNode = ARR_FrequencyCount(NodeSet, NodeSetLength, 6);
+	MatchNode = ARR_FrequencyCount(NodeSet, NodeSetLength, 3);
+	MatchNode = ARR_FrequencyCount(NodeSet, NodeSetLength, 3);
+
+	if (MatchNode == NULL)
+	{
+		// 순차탐색 결과 해당 요소가 없을 경우, 탐색에 실패했다는 메시지 출력
+		printf("Node Not Found!\n");
+	}
+	else
+	{
+		// 순차탐색 결과 해당 요소를 발견했다면, 요소의 값을 출력
+		printf("Match Node : %d\n", MatchNode->Data);
+
+		// 전진이동법으로 정렬된 배열 출력 (가장 많이 탐색된 순으로 정렬)
+		printf("\nPrinting NodeSet...\n");
+		for (int i = 0; i < NodeSetLength; i++)
+		{
+			printf("%d ", NodeSet[i].Data);
 		}
 		printf("\n");
 	}
