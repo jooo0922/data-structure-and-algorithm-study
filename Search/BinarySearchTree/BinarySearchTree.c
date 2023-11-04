f965d95b92b27dcac7a15f3588b3e36e292519d1
jooo0922 <jooo0922@gmail.com>
2023-11-04 16:01:40 +0900

    feat: 이진트리탐색 및 최솟값 노드 탐색 함수 구현

--- Search/BinarySearchTree/BinarySearchTree.c
+++ Search/BinarySearchTree/BinarySearchTree.c
@@ -48,13 +48,62 @@ void BST_DestroyTree(BSTNode* Tree)
 // 이진탐색트리 노드 탐색
 BSTNode* BST_SearchNode(BSTNode* Tree, ElementType Target)
 {
+	if (Tree == NULL)
+	{
+		// 현재 노드 NULL 체크
+		return NULL;
+	}
 
+	if (Tree->Data == Target)
+	{
+		// 현재 순회중인 노드가 목표 노드와 같은 경우,
+		// 현재 노드 주소값을 그대로 반환
+		return Tree;
+	}
+	else if (Tree->Data > Target)
+	{
+		// 현재 순회중인 노드보다 목표 노드가 더 작을 경우,
+		// 현재 노드의 왼쪽 하위 트리를 재귀적으로 다시 탐색함
+		return BST_SearchNode(Tree->Left, Target);
+	}
+	else
+	{
+		// 현재 순회중인 노드보다 목표 노드가 더 클 경우,
+		// 현재 노드의 오른쪽 하위 트리를 재귀적으로 다시 탐색함
+		return BST_SearchNode(Tree->Right, Target);
+
+	}
 }
 
 // 주어진 하위트리 내의 최솟값 노드 탐색
 BSTNode* BST_SearchMinNode(BSTNode* Tree)
 {
+	if (Tree == NULL)
+	{
+		// 현재 노드 NULL 체크
+		return NULL;
+	}
 
+	/* 
+		이진탐색트리 상에서는 
+		현재 노드보다 작은 노드를 
+		항상 왼쪽 하위 트리에 둔다는 것을 명심!
+	*/
+
+	if (Tree->Left == NULL)
+	{
+		// 현재 노드의 왼쪽 하위트리가 존재하지 않을 경우,
+		// 현재 하위트리(=현재 노드)상에서 더 작은 노드는 존재하지 않는다는 의미이므로,
+		// 현재 노드를 최솟값 노드로 반환함
+		return Tree;
+	}
+	else
+	{
+		// 현재 노드의 왼쪽 하위트리가 존재할 경우,
+		// 현재 하위트리 상에서 더 작은 노드가 존재한다는 의미이므로,
+		// 현재 노드의 왼쪽 자식노드를 전달해서 재귀적으로 최솟값 노드를 재탐색함.
+		return BST_SearchMinNode(Tree->Left);
+	}
 }
 
 // 이진탐색트리 노드 삽입
