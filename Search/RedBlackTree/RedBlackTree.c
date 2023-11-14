1651f73b4b04ae73737cee478ad25df6372ddeea
jooo0922 <jooo0922@gmail.com>
2023-11-14 22:31:51 +0900

    feat: RBT_CreateNode() 구현

--- Search/RedBlackTree/RedBlackTree.c
+++ Search/RedBlackTree/RedBlackTree.c
@@ -15,3 +15,24 @@
 	이를 찾아서 연결해야 한다는 것을 알려줌.
 */
 extern RBTNode* Nil;
+
+// 레드블랙트리 노드 생성
+RBTNode* RBT_CreateNode(ElementType NewData)
+{
+	// RBTNode 구조체 크기 만큼의 힙 메모리 할당
+	// malloc() 이 할당한 힙 메모리 주소값 반환 시, RBTNode* 타입의 포인터 주소로 형변환하여 반환.
+	RBTNode* NewNode = (RBTNode*)malloc(sizeof(RBTNode));
+
+	// 노드 구조체 각 멤버변수 초기화
+	NewNode->Parent = NULL;
+	NewNode->Left = NULL;
+	NewNode->Right = NULL;
+	NewNode->Data = NewData;
+
+	// 기본값은 검은색 노드로 초기화
+	NewNode->Color = BLACK;
+
+	// 생성한 노드의 주소값(이 담긴 포인터 변수) 반환
+	return NewNode;
+}
+
