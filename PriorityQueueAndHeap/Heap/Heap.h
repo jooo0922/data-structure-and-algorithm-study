995920b69cea96de9cb1449d31ffd13d9462e2b0
jooo0922 <jooo0922@gmail.com>
2023-11-27 00:25:44 +0900

    feat: 힙 자료구조 연산 함수 선언

--- PriorityQueueAndHeap/Heap/Heap.h
+++ PriorityQueueAndHeap/Heap/Heap.h
@@ -29,4 +29,13 @@ typedef struct tagHeap
 	int UsedSize; // 힙 노드 배열 실제 노드 개수
 } Heap;
 
+Heap* HEAP_Create(int InitialSize); // 힙 구조체 생성
+void HEAP_Destroy(Heap* H); // 힙 구조체 메모리 해제
+void HEAP_Insert(Heap* H, ElementType NewData); // 힙 노드 삽입
+void HEAP_DeleteMin(Heap* H, HeapNode* Root); // 힙 최솟값 노드 삭제 (= 뿌리노드 삭제)
+int HEAP_GetParent(int Index); // 주어진 노드의 부모노드 탐색
+int HEAP_GetLeftChild(int Index); // 주어진 노드의 왼쪽 자식노드 탐색 (오른쪽 자식노드 = 왼쪽 자식노드 위치 + 1)
+void HEAP_SwapNodes(Heap* H, int Index1, int Index2); // 힙의 두 노드 위치 교환
+void HEAP_PrintNodes(Heap* H); // 힙 전체 출력
+
 #endif // !HEAP_H
