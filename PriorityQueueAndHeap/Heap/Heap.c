1c0c69f1ecfaa9307ecc82f64da9412c1c5f19a8
jooo0922 <jooo0922@gmail.com>
2023-11-27 22:41:12 +0900

    feat: HEAP_DeleteMin() 함수 구현

--- PriorityQueueAndHeap/Heap/Heap.c
+++ PriorityQueueAndHeap/Heap/Heap.c
@@ -73,7 +73,89 @@ void HEAP_Insert(Heap* H, ElementType NewData)
 // 힙 최솟값 노드 삭제 (= 뿌리노드 삭제)
 void HEAP_DeleteMin(Heap* H, HeapNode* Root)
 {
+	// 부모노드 인덱스를 0(= 뿌리노드)으로 초기화
+	int ParentPosition = 0;
 
+	// 부모의 왼쪽 / 오른쪽 자식노드 인덱스를 0으로 초기화
+	int LeftPosition = 0;
+	int RightPosition = 0;
+
+	// 제거할 뿌리노드 데이터를 포인터 매개변수 Root 에 복사하여 저장해 둠.
+	memcpy(Root, &H->Nodes[0], sizeof(HeapNode));
+
+	// 힙의 최솟값 노드(= 뿌리노드)의 데이터를 모두 0으로 초기화함 -> '뿌리노드 삭제' (p.310)
+	memset(&H->Nodes[0], 0, sizeof(HeapNode));
+
+	// 힙에서 (뿌리)노드를 하나 제거했으니 실제 노드 갯수를 -1 차감
+	H->UsedSize--;
+
+	// 힙의 최고 깊이 가장 우측 노드(H->UsedSize)와 0으로 초기화된 뿌리노드의 위치를 교체함 (p.311)
+	HEAP_SwapNodes(H, 0, H->UsedSize);
+
+	// 힙의 최고 깊이 가장 우측 노드가 올라간 뿌리노드 자리의 양쪽 자식노드 인덱스 저장
+	LeftPosition = HEAP_GetLeftChild(0);
+	RightPosition = LeftPosition + 1; // 항상 배열 기반 Heap 에서는 '오른쪽 자식노드 인덱스 = 왼쪽 자식노드 인덱스 + 1' 임을 기억할 것! (p.313)
+
+	// 힙 순서 속성 (= 힙의 모든 노드는 부모노드보다 크다)를 만족할 때까지 자식노드와 부모노드 위치 교체를 반복
+	while (1)
+	{
+		// 부모와 위치를 바꿀 자식노드 인덱스 저장
+		int SelectedChild = 0;
+
+		if (LeftPosition >= H->UsedSize)
+		{
+			// 왼쪽 자식노드 인덱스가 힙의 최고 깊이 가장 우측 노드 인덱스보다 크다면,
+			// 왼쪽 자식은 이미 현재 힙 영역을 벗어난 노드 위치이며, 오른쪽 자식 역시 마찬가지이다.
+			// 따라서, 현재 힙 영역을 벗어난 노드의 위치값이므로, 반복문을 중단!
+			break;
+		}
+
+		if (RightPosition >= H->UsedSize)
+		{
+			// 상단의 if 조건문을 통과해서 두 번째 if block 에 도달했다면, 왼쪽 자식노드는 힙 영역 내에 존재하겠지만,
+			// 오른쪽 자식노드가 힙 영역 바깥에 위치하고 있다는 뜻. -> 즉, '왼쪽 자식노드가 힙 최고 깊이 가장 우측 노드' 에 해당하는 케이스!
+			SelectedChild = LeftPosition;
+		}
+		else
+		{
+			// 왼쪽 / 오른쪽 자식노드 모두 힙 영역 안쪽에 존재하고 있는 위치값일 경우
+
+			// 두 자식노드 중 더 작은 값의 노드를 부모노드와 교체할 노드로 선택 (p.310 2번 뒷처리 과정)
+			if (H->Nodes[LeftPosition].Data > H->Nodes[RightPosition].Data)
+			{
+				SelectedChild = RightPosition;
+			}
+			else
+			{
+				SelectedChild = LeftPosition;
+			}
+		}
+
+		if (H->Nodes[SelectedChild].Data < H->Nodes[ParentPosition].Data)
+		{
+			// 교체할 자식노드가 부모노드보다 작다면, 힙 순서 속성이 깨진 것이므로, 둘의 위치를 교체함 (p.311)
+			HEAP_SwapNodes(H, ParentPosition, SelectedChild);
+			ParentPosition = SelectedChild;
+		}
+		else
+		{
+			// 힙 순서 속성이 깨지지 않았다면 반복문 중단
+			break;
+		}
+
+		// 다음 순회에서 비교할 왼쪽 / 오른쪽 자식노드 위치값 업데이트
+		LeftPosition = HEAP_GetLeftChild(ParentPosition);
+		RightPosition = LeftPosition + 1;
+	} // 반복문 탈출!
+
+	if (H->UsedSize < (H->Capacity / 2))
+	{
+		// 반복문을 순회하며 최솟값 노드 제거 후,
+		// 힙 최대 용량을 확인해봤더니, 실제 힙 노드 배열의 개수가 절반도 못미친다면? -> 메모리 낭비!
+		// 따라서, 힙 노드 배열에 할당된 메모리를 절반 수준으로 낮춰서 재할당함!
+		H->Capacity /= 2;
+		H->Nodes = (HeapNode*)realloc(H->Nodes, sizeof(HeapNode) * H->Capacity);
+	}
 }
 
 // 주어진 노드의 부모노드 탐색
@@ -131,6 +213,21 @@ void HEAP_PrintNodes(Heap* H)
 	dest: 복사한 데이터가 저장될 목적지를 가리키는 포인터입니다.
 	src: 복사할 데이터를 가리키는 포인터입니다.
 	n: 복사할 바이트 수를 나타내는 size_t 타입의 정수입니다.
+*/
+
+/*
+	void *memset(void *ptr, int value, size_t num);
+
+	'memory set' 의 줄임말로,
+	주로 배열이나 구조체와 같은 데이터 구조의 메모리 영역을
+	지정해준 값으로 초기화하는 데 사용됨.
+
+	ptr: 초기화할 메모리 영역의 시작 포인터
+	value: 메모리 영역을 초기화할 값
+	num: 초기화할 바이트 수
+
+	ptr이 가리키는 메모리 영역을
+	value 값으로 num 바이트만큼 채운다고 이해하면 됨.
 */
 
 /*
