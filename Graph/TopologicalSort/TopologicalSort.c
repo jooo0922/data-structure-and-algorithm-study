b6bfc701c2bf96a942c33330631b416a410bf9ab
jooo0922 <jooo0922@gmail.com>
2024-01-06 23:10:19 +0900

    feat: TS_DFS() 함수 구현

--- Graph/TopologicalSort/TopologicalSort.c
+++ Graph/TopologicalSort/TopologicalSort.c
@@ -17,5 +17,49 @@ void TopologicalSort(Vertex* V, Node** List)
 // 깊이 우선 탐색 및 리스트에 헤드노드 추가
 void TS_DFS(Vertex* V, Node** List)
 {
+	// 깊이 우선 탐색 결과 리스트에 헤드노드로 추가할 정점 노드의 포인터 변수 초기화 
+	Node* NewHead = NULL;
 
+	// 간선 구조체를 캐싱할 포인터 변수 초기화
+	Edge* E = NULL;
+
+	// 현재 방문한 정점을 '방문했음' 표시
+	V->Visited = Visited;
+
+	// 현재 방문한 정점의 인접 정점들의 리스트 (정확히는, 간선들의 리스트) 캐싱
+	E = V->AdjacencyList;
+
+	// 현재 방문한 정점의 인접 정점들(간선들)을 순회함
+	while (E != NULL)
+	{
+		// 인접 정점(== 간선의 끝점(E->Target))을 아직 방문하지 않은 경우, 깊이 우선 탐색을 재귀 호출함
+		if (E->Target != NULL && E->Target->Visited == NotVisited)
+		{
+			DFS(E->Target);
+		}
+
+		// 다음 인접 정점을 순회하기 위해 캐싱된 간선을 업데이트함.
+		E = E->Next;
+	}
+
+	/*
+		while 문 이후의 코드들은
+		E != NULL 인 조건을 먼저 만족하는 순으로,
+
+		즉, 더 이상 방문할 인접 정점이 (정확히는 진출 간선) 존재하지 않는
+		정점들부터 먼저 실행될 것임.
+
+		따라서, p.411 예제 삽화로 따지면,
+		가장 먼저 찾아낸 '더 이상 방문할 인접 정점이 없는 정점' 인 H 부터
+		while 문 이후의 코드들이 실행된다는 뜻!
+	*/
+
+	// 깊이 우선 탐색 결과, '더 이상 방문할 인접 정점이 없는 정점' 의 데이터 출력
+	printf("%d ", V->Data);
+
+	// '더 이상 방문할 인접 정점이 없는 정점' 을 링크드리스트의 노드로 만듦.
+	NewHead = SLL_CreateNode(V);
+
+	// 해당 노드를 링크드리스트의 헤드노드로 삽입
+	SLL_InsertNewHead(List, NewHead);
 }
