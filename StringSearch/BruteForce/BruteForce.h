12a7bbb666a9657385b519a1f3b88612231ec6c2
jooo0922 <jooo0922@gmail.com>
2024-01-30 01:04:30 +0900

    feat: BruteForce() 함수 프로토타입 선언

---
+++ StringSearch/BruteForce/BruteForce.h
@@ -0,0 +1,13 @@
+#ifndef BRUTEFORCE_H
+#define BRUTEFORCE_H
+/*
+	여기서 사용된 #ifndef ~ #endif 전처리기는
+	헤더파일의 헤더가드를 처리해주는 방법 중 하나였지?
+
+	cpp-study > TBCppStudy > Chapter1_12 > add.h 필기 참고!
+*/
+
+// 고지식한 탐색 알고리즘
+int BruteForce(char* Text, int TextSize, int Start, char* Pattern, int PatternSize);
+
+#endif // !BRUTEFORCE_H
