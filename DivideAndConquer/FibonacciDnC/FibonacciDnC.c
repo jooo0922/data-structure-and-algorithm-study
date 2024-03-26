21c51abc791cb18414b89c5cd1b5bafe7e4f7237
jooo0922 <jooo0922@gmail.com>
2024-03-26 23:58:49 +0900

    feat: 2*2 정방행렬 곱셈 계산 함수 구현

--- DivideAndConquer/FibonacciDnC/FibonacciDnC.c
+++ DivideAndConquer/FibonacciDnC/FibonacciDnC.c
@@ -9,3 +9,17 @@ typedef struct tagMatrix2x2
 	// 2차원 배열로 선언
 	ULONG Data[2][2];
 } Matrix2x2;
+
+// 2*2 정방행렬 곱셈 계산 함수
+Matrix2x2 Matrix2x2_Multiply(Matrix2x2 A, Matrix2x2 B)
+{
+	Matrix2x2 C;
+
+	C.Data[0][0] = A.Data[0][0] * B.Data[0][0] + A.Data[0][1] * B.Data[1][0];
+	C.Data[0][1] = A.Data[0][0] * B.Data[1][0] + A.Data[0][1] * B.Data[1][1];
+
+	C.Data[1][0] = A.Data[1][0] * B.Data[0][0] + A.Data[1][1] * B.Data[1][0];
+	C.Data[1][1] = A.Data[1][0] * B.Data[1][0] + A.Data[1][1] * B.Data[1][1];
+
+	return C;
+}
