2e214b61a375a17466000b6cb1a01cb77ad03103
jooo0922 <jooo0922@gmail.com>
2024-04-22 00:21:17 +0900

    feat: 헤더파일 include

---
+++ DynamicProgramming/LCSDP/LCSDP.c
@@ -0,0 +1,3 @@
+#include <stdio.h> // printf(), scanf() 같은 입출력 함수 선언이 포함된 헤더파일
+#include <string.h> // strlen(), strcpy() 같은 문자열 처리 함수 선언이 포함된 헤더파일
+#include <stdlib.h> // malloc(), free() 같은 메모리 할당 함수 선언이 포함된 헤더파일
