5b82aad0430fa5f6417ae03630eadf5e86bbb503
jooo0922 <jooo0922@gmail.com>
2024-05-20 01:51:49 +0900

    feat: Huffman.h 헤더파일 생성

---
+++ Greedy/Huffman/Huffman.h
@@ -0,0 +1,15 @@
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+/*
+	여기서 사용된 #ifndef ~ #endif 전처리기는
+	헤더파일의 헤더가드를 처리해주는 방법 중 하나였지?
+
+	cpp-study > TBCppStudy > Chapter1_12 > add.h 필기 참고!
+*/
+
+#include <stdio.h> // printf(), scanf() 같은 입출력 함수 선언이 포함된 헤더파일
+#include <stdlib.h> // malloc(), free() 같은 메모리 할당 함수 선언이 포함된 헤더파일
+#include "PriorityQueue.h" // 허프만 트리 구축에 사용할 우선순위 큐 자료구조가 포함된 헤더파일
+// -> 헤더파일에 포함되어 있어야, 이를 가져다 쓰는 .c 파일들에서 사용할 때 에러가 안남.
+
+#endif // !HUFFMAN_H
