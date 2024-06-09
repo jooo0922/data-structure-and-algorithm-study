308297c86bc57340e53f4b924d71b61ce8baa1ad
jooo0922 <jooo0922@gmail.com>
2024-06-09 23:01:15 +0900

    feat: 헤더가드 및 헤더파일 include 선언

---
+++ Backtracking/MazeSolver/MazeSolver.h
@@ -0,0 +1,14 @@
+#ifndef MAZESOLVER
+#define MAZESOLVER
+/*
+	여기서 사용된 #ifndef ~ #endif 전처리기는
+	헤더파일의 헤더가드를 처리해주는 방법 중 하나였지?
+
+	cpp-study > TBCppStudy > Chapter1_12 > add.h 필기 참고!
+*/
+
+#include <stdio.h> // printf(), scanf() 같은 입출력 함수 선언이 포함된 헤더파일
+#include <stdlib.h> // malloc(), free() 같은 메모리 할당 함수 선언이 포함된 헤더파일
+#include <string.h> // strlen(), strcpy() 같은 문자열 처리 함수 선언이 포함된 헤더파일
+
+#endif // !MAZESOLVER
