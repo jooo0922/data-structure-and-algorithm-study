8118242e370171aa5b20bb11c2e32607592c8f63
jooo0922 <jooo0922@gmail.com>
2024-06-09 23:16:10 +0900

    feat: 매크로 상수 선언

--- Backtracking/MazeSolver/MazeSolver.h
+++ Backtracking/MazeSolver/MazeSolver.h
@@ -11,4 +11,13 @@
 #include <stdlib.h> // malloc(), free() 같은 메모리 할당 함수 선언이 포함된 헤더파일
 #include <string.h> // strlen(), strcpy() 같은 문자열 처리 함수 선언이 포함된 헤더파일
 
+
+/* 매크로 상수 선언 */
+
+// 입력된 파일로부터 미로 데이터를 한 번에 읽어들일 수 있는 최대 길이 (fgets() 로 읽음!)
+#define MAX_BUFFER 1024
+
+// 입력된 파일로부터 읽어들인 미로 데이터의 전체 열(ColunmSize) 개수를 저장하는 변수의 초기값
+#define INIT_VALUE -1
+
 #endif // !MAZESOLVER
