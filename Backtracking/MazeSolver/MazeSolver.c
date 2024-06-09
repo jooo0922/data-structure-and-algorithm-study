1d11d82881ae152b0f37f152503c676cb65ad5ee
jooo0922 <jooo0922@gmail.com>
2024-06-10 00:37:40 +0900

    feat: MoveTo() 함수 구현

--- Backtracking/MazeSolver/MazeSolver.c
+++ Backtracking/MazeSolver/MazeSolver.c
@@ -97,7 +97,85 @@ int Solve(MazeInfo* Maze)
 */
 int MoveTo(MazeInfo* Maze, Position* Current, int Direction)
 {
+	// 현 위치에서 이동 가능한 다음 부분해 목록을 순회할 때 사용할 인덱스 초기화
+	int i = 0;
 
+	// 현 위치에서 이동 가능한 모든 방향(북, 남, 동, 서 순서)의 enum 을 배열로 저장
+	int Dirs[] = { NORTH, SOUTH, EAST, WEST };
+
+	// GetNextStep() 함수에서 다음 경로의 위치값을 캐싱하기 위해 넘겨줄 Position 구조체 변수 생성
+	Position Next;
+
+	/*
+		현재 위치가 출구인 경우(= 진짜 해를 찾은 경우),
+		MoveTo() 함수의 재귀 호출을 return 하여 Backtracking 수행
+
+		-> p.613 ~ 614, p.616 4번 과정 참고
+	*/
+	if (Maze->Data[Current->Y][Current->X] == GOAL)
+	{
+		return SUCCEED;
+	}
+
+	/*
+		현재 위치는 이미 지나온 경로에 해당하므로,
+		현재 위치는 MARKED('+') 로 표시함.
+
+		-> 이래야 GetNextStep() 함수에서 새로운 이동 경로를 찾을 때,
+		이미 지나온 경로를 피해갈 수 있음.
+	*/
+	Maze->Data[Current->Y][Current->X] = MARKED;
+
+	// 현 위치의 북, 남, 동, 서 방향으로 순회하며 이동 가능한 다음 부분해를 검사함
+	for (i = 0; i < 4; i++)
+	{
+		if (GetNextStep(Maze, Current, Dirs[i], &Next) == FAIL)
+		{
+			/*
+				각 방향의 후보해(= 다음 이동 경로)를 검사해서 FAIL 이 나왔다면, 
+				어떤 이유에 의해서건 해당 방향은 이동 불가 판정을 받은 것이므로,
+				다음 방향을 검사할 수 있도록 for 문의 다음 순회로 넘어감.
+
+				-> p.616 2, 3번 과정에 해당
+			*/
+			continue;
+		}
+
+		/*
+			for 문의 다음 순회로 넘어가지 않고 여기까지 왔다는 것은,
+			방금 검사한 후보해(= 다음 이동 경로)가 이동 가능한 방향임이 확인되었다는 뜻이므로,
+
+			해당 방향으로 이동
+			(참고로, GetNextStep() 함수에 의해 
+			Next 에 이미 이동한 위치값이 덮어쓰여 있음)
+			해서 MoveTo() 함수를 재귀 호출함
+
+			-> p.616 3번 과정에 해당
+
+			(근데 int Direction 매개변수는 함수 내에서 아예 사용 자체를 안하는데,
+			왜 NORTH 로 넘겨준거지? P.616 의 1번 과정을 의식한건가...
+			아무튼 별 의미 없는 매개변수 전달인 것 같음...)					
+		*/
+		if (MoveTo(Maze, &Next, NORTH) == SUCCEED)
+		{
+			return SUCCEED;
+		}
+	}
+
+	/*
+		위 for 문을 모두 순회하고 나서도
+		return SUCCEED 를 하지 못했다는 것은,
+
+		현재 위치(Current)의 북, 남, 동, 서 방향 모두 
+		이동 불가한 경로라는 뜻이므로, 현재 위치는 '막다른 길' 이라고 보면 됨.
+
+		따라서, 현재 위치를 다시 WAY(' ')로 표시하고,
+		return FAIL; 로 재귀 호출을 종료함으로써,
+		상위 부분해로 Backtracking 을 수행함 -> p.614 참고
+	*/
+	Maze->Data[Current->Y][Current->X] = WAY;
+
+	return FAIL;
 }
 
 // 각 후보해들(= 다음 이동 경로)의 이동 가능 여부를 실제로 검사
