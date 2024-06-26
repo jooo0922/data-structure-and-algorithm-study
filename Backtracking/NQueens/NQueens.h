148fa357e588199cd0cc526b114cc90c5a13ce01
jooo0922 <jooo0922@gmail.com>
2024-06-27 00:22:53 +0900

    feat: 함수 프로토타입 선언

--- Backtracking/NQueens/NQueens.h
+++ Backtracking/NQueens/NQueens.h
@@ -10,4 +10,15 @@
 #include <stdio.h> // printf(), scanf() 같은 입출력 함수 선언이 포함된 헤더파일
 #include <stdlib.h> // malloc(), free() 같은 메모리 할당 함수 선언이 포함된 헤더파일
 
+/* 함수 프로토타입 선언 */
+
+// 백트래킹으로 찾은 N개의 퀸 해 출력
+void PrintSolution(int Columns[], int NumberOfQueens);
+
+// 새로 놓은 퀸(새로운 부분해)가 기존에 놓인 퀸(이전 상위 부분해)을 위협하는지 여부 검사
+int IsThreatened(int Columns[], int NewRow);
+
+// 백트래킹으로 N개의 퀸의 모든 해 탐색
+void FindSolutionForQueen(int Columns[], int Row, int NumberOfQueens, int* SolutionCount)
+
 #endif // !NQUEENS_H
