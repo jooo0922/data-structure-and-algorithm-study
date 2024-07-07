7269bc0221d8af552f96c1872e5ef477df3fda34
jooo0922 <jooo0922@gmail.com>
2024-07-07 10:13:25 +0900

    chore: Sudoku.h 헤더파일 생성

---
+++ Backtracking/Sudoku/Sudoku.h
@@ -0,0 +1,12 @@
+#ifndef SUDOKU_H
+#define SUDOKU_H
+/*
+	여기서 사용된 #ifndef ~ #endif 전처리기는
+	헤더파일의 헤더가드를 처리해주는 방법 중 하나였지?
+
+	cpp-study > TBCppStudy > Chapter1_12 > add.h 필기 참고!
+*/
+
+#include <stdio.h> // printf(), scanf() 같은 입출력 함수 선언이 포함된 헤더파일
+
+#endif // !SUDOKU_H
